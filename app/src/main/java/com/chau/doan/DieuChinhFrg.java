b04dfd393f3386223a107da5f80505d354e6d693
hodung2809 <ngandung1109@gmial.com>
2020-11-28 15:38:48 +0700

    update2

--- app/src/main/java/com/chau/doan/DieuChinhFrg.java
+++ app/src/main/java/com/chau/doan/DieuChinhFrg.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DieuChinhFrg extends FragmentPagerAdapter {
-    private List<Fragment> fragmentList = new ArrayList<>();
-    private List<String> titlist = new ArrayList<>();
+    private List<Fragment> fragmentList1 = new ArrayList<>();
+    private List<String> titlist1 = new ArrayList<>();
     public DieuChinhFrg(@NonNull FragmentManager fm) {
         super(fm);
     }
@@ -19,23 +19,23 @@ public class DieuChinhFrg extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return fragmentList.get(position);
+        return fragmentList1.get(position);
     }
 
     @Override
     public int getCount() {
-        return fragmentList.size();
+        return fragmentList1.size();
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return titlist.get(position);
+        return titlist1.get(position);
     }
 
     public void addTab(Fragment fragment, String title)
     {
-        fragmentList.add(fragment);
-        titlist.add(title);
+        fragmentList1.add(fragment);
+        titlist1.add(title);
     }
 }
